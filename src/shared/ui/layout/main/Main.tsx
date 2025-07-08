70ed945bfa23d05872a6520a1ba2ac30f02c8783
Dmitry <khv1702@gmail.com>
2025-07-08 03:13:29 +0300

    Add main func component

---
+++ src/shared/ui/layout/main/Main.tsx
@@ -0,0 +1,11 @@
+import type { FC } from 'react';
+import styles from './Main.module.css';
+
+export const Main: FC = () => {
+
+  return (
+    <main className={styles.main}>
+
+    </main>
+  );
+};
