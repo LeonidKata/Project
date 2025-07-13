e0324e607b92385e5be4045c36d5a591c59ddabf
Dmitry <khv1702@gmail.com>
2025-07-13 03:01:33 +0300

    Add root api config

---
+++ src/store/rootApi.ts
@@ -0,0 +1,21 @@
+import { createApi, fetchBaseQuery, retry } from '@reduxjs/toolkit/query/react';
+
+import { API_URL } from '../shared/api/rootApi.config';
+
+export const rootApi = createApi({
+  reducerPath: 'rootApi',
+  baseQuery: retry(fetchBaseQuery({ baseUrl: API_URL }), {
+    maxRetries: 1,
+  }),
+  endpoints: (builder) => ({
+    // Тестовый эндпоинт получения всех бронирований
+    getBookings: builder.query<string, void>({
+      query: () => '/bookings',
+      transformResponse: (response: { content: any }) => response.content,
+    }),
+    // Другие эндпоинты
+  }),
+});
+
+export const { useGetBookingsQuery } = rootApi;
+export default rootApi;
