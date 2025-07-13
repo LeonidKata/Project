2128332ec98efe6ef3def3d7cef172704bfc356f
Dmitry <khv1702@gmail.com>
2025-07-13 03:03:33 +0300

    Fix

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,9 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 
-import App from '@/App.tsx';
-import '@/index.css';
-import { store } from '@/store/index.ts';
+import App from './App.tsx';
+import './index.css';
+import { store } from './store';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
