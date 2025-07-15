54000d4193b22da1e5597745016b3aee3cf43d7d
Alex-Bengardt <alexandragorky@yandex.ru>
2025-07-15 13:56:24 +0300

    Базовая настройка router.tsx

---
+++ src/router.tsx
@@ -0,0 +1,24 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { NotFound } from "./pages/NotFound";
+import App from "./App";
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: '*',
+        element: <NotFound />
+      },
+    ],
+  },
+]);
+
+export const Router = () => <RouterProvider router={router} />
