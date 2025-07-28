29c029920f2a816d47bd62cbf9caf475b8e31f64
LeoLis <l.lisyanskiy@kata.academy>
2025-07-28 18:46:28 +0300

    Fix: Added prettier

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,15 +1,16 @@
-import { Outlet } from 'react-router-dom'
-import './App.css'
+import { Outlet } from 'react-router-dom';
+
+import './App.css';
 
 function App() {
-
+  console.log();
   return (
     <div>
       <main>
         <Outlet />
       </main>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
