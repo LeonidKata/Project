ccfed0e53d45d78e579329fd3f118df5af2cff23
sergey <fastrebit@gmail.com>
2025-07-30 16:29:04 +0300

    fix

--- src/shared/ui/Table/Table.tsx
+++ src/shared/ui/Table/Table.tsx
@@ -1,14 +1,25 @@
-import type { FC } from 'react';
 import { Table as AntTable } from 'antd';
 import type { TableProps } from 'antd';
+
 import styles from './Table.module.css';
 
-export const Table: FC<TableProps> = ({ ...props }) => {
+interface Props<T> extends TableProps<T> {
+  columns: TableProps<T>['columns'];
+}
 
+export const Table = <T extends object>({ ...props }: Props<T>) => {
   return (
     <div className={styles.container}>
-      <AntTable {...props} bordered className="table"
-                pagination={{ className: 'tablePagination', position: ['bottomLeft'], showSizeChanger: false }} />
+      <AntTable
+        {...props}
+        bordered
+        className="table"
+        pagination={{
+          className: 'tablePagination',
+          position: ['bottomLeft'],
+          showSizeChanger: false,
+        }}
+      />
     </div>
   );
 };
